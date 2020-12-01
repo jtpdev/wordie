51384e894385c02dfdd2d87b96ebc90f046d33b2
Jimmy Porto <ireturnerd@gmail.com>
2020-11-30 23:29:29 -0300

    First step

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          components: './src/components',
+          state: './src/state',
+          services: "./src/services",
+          assets: "./src/assets",
+          utils: "./src/utils",
+        },
+      }
+    ],
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import App from './App'
+
+export default App;
